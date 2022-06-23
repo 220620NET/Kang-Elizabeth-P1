4baefade2f08acd27ae264c7d492217ee0440f7f
Elizabeth Kang <liz.kang@hey.com>
2022-06-23 17:41:37 -0500

    Added more to Ticket class

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -3,9 +3,17 @@ namespace Models;
 public class Ticket
 {
     public string ID {get; set;}
-    public string Author {get; set;}
-    public string Resolver {get; set;}
-    public string Status {get; set;}
-    public string Amount {get; set;}
+    public string author {get; set;}
+    public string resolver {get; set;}
+    public string status {get; set;}
+    public decimal amount {get; set;}
 
+    public Ticket(string ID, string author, string resolver, string status, decimal status)
+    {
+        this.ID = ID;
+        this.author = author;
+        this.resolver = resolver;
+        this.status = status;
+        this.amount = amount;
+    }
 }

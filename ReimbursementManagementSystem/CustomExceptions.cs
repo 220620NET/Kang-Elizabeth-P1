3eb5f13bb4b5285c7ddb44c30632e7c2e2f7b3c6
Elizabeth Kang <liz.kang@hey.com>
2022-06-28 09:28:39 -0500

    Renamed exceptions in CustomExceptions class

--- ReimbursementManagementSystem/CustomExceptions.cs
+++ ReimbursementManagementSystem/CustomExceptions.cs
@@ -1,29 +1,29 @@
 namespace CustomExceptions;
-public class ResourceNotFound : System.Exception
+public class ResourceNotFoundException : System.Exception
 {
-    public ResourceNotFound() { }
-    public ResourceNotFound(string message) : base(message) { }
-    public ResourceNotFound(string message, System.Exception inner) : base(message, inner) { }
-    protected ResourceNotFound(
+    public ResourceNotFoundException() { }
+    public ResourceNotFoundException(string message) : base(message) { }
+    public ResourceNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+    protected ResourceNotFoundException(
         System.Runtime.Serialization.SerializationInfo info,
         System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
 
-public class UsernameNotAvailable : System.Exception
+public class UsernameNotAvailableException : System.Exception
 {
-    public UsernameNotAvailable() { }
-    public UsernameNotAvailable(string message) : base(message) { }
-    public UsernameNotAvailable(string message, System.Exception inner) : base(message, inner) { }
-    protected UsernameNotAvailable(
+    public UsernameNotAvailableException() { }
+    public UsernameNotAvailableException(string message) : base(message) { }
+    public UsernameNotAvailableException(string message, System.Exception inner) : base(message, inner) { }
+    protected UsernameNotAvailableException(
         System.Runtime.Serialization.SerializationInfo info,
         System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
-public class InvalidCredentials : System.Exception
+public class InvalidCredentialsException : System.Exception
 {
-    public InvalidCredentials() { }
-    public InvalidCredentials(string message) : base(message) { }
-    public InvalidCredentials(string message, System.Exception inner) : base(message, inner) { }
-    protected InvalidCredentials(
+    public InvalidCredentialsException() { }
+    public InvalidCredentialsException(string message) : base(message) { }
+    public InvalidCredentialsException(string message, System.Exception inner) : base(message, inner) { }
+    protected InvalidCredentialsException(
         System.Runtime.Serialization.SerializationInfo info,
         System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }

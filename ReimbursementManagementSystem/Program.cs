508bc51efa82f932ebc13ecbd2ee7b4c10e76404
Elizabeth Kang <liz.kang@hey.com>
2022-06-29 15:15:16 -0500

    Tested User class in program.cs

--- ReimbursementManagementSystem/Program.cs
+++ ReimbursementManagementSystem/Program.cs
@@ -4,3 +4,7 @@ using CustomExceptions;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+User user1 = new User("test", "test", "test", Role.Manager);
+
+Console.WriteLine(user1.userName);

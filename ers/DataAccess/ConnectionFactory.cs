dfa977bc223b7fc6eaf3606f8a962c4256bb48f2
Elizabeth Kang <liz.kang@hey.com>
2022-07-15 12:20:47 -0500

    Updated to use JSON for Azure DB

--- ers/DataAccess/ConnectionFactory.cs
+++ ers/DataAccess/ConnectionFactory.cs
@@ -5,13 +5,16 @@ namespace DataAccess;
 public class ConnectionFactory
 {
     private static ConnectionFactory? _instance;
-    private readonly static string _connectionString = File.ReadAllText("../DataAccess/connectionString.txt");
-    private ConnectionFactory() {}
-    public static ConnectionFactory GetInstance()
+    private readonly string _connectionString;
+    private ConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public static ConnectionFactory GetInstance(string connectionString)
     {
         if(_instance == null)
         {
-            _instance = new ConnectionFactory();
+            _instance = new ConnectionFactory(connectionString);
         }
         return _instance;
     }

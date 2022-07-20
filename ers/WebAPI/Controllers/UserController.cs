09c462e7c92ac79a60e2fa640debd5509b3ebacc
Elizabeth Kang <liz.kang@hey.com>
2022-07-19 22:16:39 -0500

    GetAllUsers completed

--- ers/WebAPI/Controllers/UserController.cs
+++ ers/WebAPI/Controllers/UserController.cs
@@ -7,5 +7,26 @@ namespace WebAPI.Controller;
 
 public class UserController
 {
-    
+    private readonly UserServices _Services;
+    public UserController(UserServices services)
+    {
+        _Services = services;
+    }
+
+    /// <summary>
+    /// Controller for getting all users
+    /// </summary>
+    /// <returns>Status code 202 for success get all users</returns>
+    public IResult GetAllUsers()
+    {
+        try
+        {
+            List<User> ListUsers = _Services.GetAllUsers();
+            return Results.Accepted("/users", ListUsers);
+        }
+        catch(ResourceNotFoundException)
+        {
+            return Results.NotFound("There are no users");
+        }
+    }
 }

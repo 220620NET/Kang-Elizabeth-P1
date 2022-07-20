f1fc18a5091ae286fcd67d0d05b983952164e799
Elizabeth Kang <liz.kang@hey.com>
2022-07-19 21:29:16 -0500

    Completed TicketController

--- ers/WebAPI/Controllers/TicketController.cs
+++ ers/WebAPI/Controllers/TicketController.cs
@@ -22,7 +22,7 @@ public class TicketController
     {
         try
         {
-            bool all = _Services.SubmitReimbursement(newTicket);
+            bool test = _Services.SubmitReimbursement(newTicket);
             return Results.Accepted("/submit", _Services.GetReimbursementByUserID(newTicket.authorID));
         }
         catch(ResourceNotFoundException)
@@ -34,6 +34,90 @@ public class TicketController
             return Results.Conflict("No user with that ID");
         }
     }
-    
 
+    /// <summary>
+    /// Controller for getting a ticket
+    /// </summary>
+    /// <param name="newTicket">The ticket object for updating an existing ticket</param>
+    /// <returns>Status code 202 Ticket is update</returns>
+    public IResult Process(Ticket newTicket)
+    {
+        try
+        {
+            bool test = _Services.UpdateReimbursement(newTicket);
+            if(test)
+            {
+                return Results.Accepted("/process", _Services.GetReimbursementByID(newTicket.ID));
+            }
+            return Results.BadRequest();
+        }
+        catch(ResourceNotFoundException)
+        {
+            return Results.Conflict("The ticket could not be updated");
+        }
+        catch (UsernameNotAvailableException)
+            {
+                return Results.Conflict("The user with that ID does not exist.");
+            }
+    }
+
+    /// <summary>
+    /// Controller to get a tickets with corresponding status
+    /// </summary>
+    /// <param name="stringStatus">The status of the ticket</param>
+    /// <returns>Status code 202 and List of Tickets with the corresponding status</returns>
+    public IResult GetTicketsByStatus(string stringStatus)
+    {
+        Status state = new Ticket().StringToStatus(stringStatus);
+        try
+        {
+            List<Ticket> listTickets = _Services.GetReimbursementByStatus(state);
+            if(listTickets.Count == 0)
+            {
+                throw new ResourceNotFoundException();
+            }
+            return Results.Accepted("/tickets/status/{state}", listTickets);
+        }
+        catch (ResourceNotFoundException)
+        {
+            return Results.BadRequest("There are no tickets that are " + stringStatus);
+        }
+    }
+
+    /// <summary>
+    /// Conttroller to get tickets with author ID
+    /// </summary>
+    /// <param name="authorID">The author ID used to search tickets</param>
+    /// <returns>Status code 202 and tickets with author</returns>
+    public IResult GetTicketsByAuthor(int authorID)
+    {
+        try
+        {
+            List<Ticket> listTickets = _Services.GetReimbursementByUserID(authorID);
+            return Results.Accepted("/tickets/author/" + authorID, listTickets);
+        }
+        catch (ResourceNotFoundException)
+        {
+            return Results.BadRequest("The user has not authored any tickets");
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="ticketID"></param>
+    /// <returns>Status code 202 and </returns>
+    public IResult GetTicketByTicketID(int ticketID)
+    {
+        try
+        {
+            Ticket ticket = _Services.GetReimbursementByID(ticketID);
+            return Results.Accepted("tickets/id/" + ticketID);
+        }
+        catch (System.Exception)
+        {
+            return Results.BadRequest("The ticket doesn't exist");
+            throw;
+        }
+    }
 }
